fe56cdf8a40baacdde796016c4530aaf82833331
Sebastian Dall <semoda@bio.aau.dk>
2022-12-16 12:32:23 +0100

    removed tests. Functions moved to mplibrary

--- addtest.R
+++
@@ -1,9 +0,0 @@
-library("testthat")
-library("usethis")
-library("renv")
-
-
-#usethis::use_testthat()
-# use tset_file / test_dir to run tests
-
-test_file("tests/testthat/test-metadata_functions.R")
